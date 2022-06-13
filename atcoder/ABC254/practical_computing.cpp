e44720e02a46f971bfb1108c51353d238ef47fe4
shion noguchi <shion24stat@gmail.com>
2022-06-13 07:50:36 +0000

    submit version

--- atcoder/ABC254/practical_computing.cpp
+++ atcoder/ABC254/practical_computing.cpp
@@ -1,31 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> print_n(int i, int j, vector<int> pre_nums){
-
-    vector<int> return_vec(i + 1);
-    if(j == 0 || j == i) {
-        return_vec.at(j) = 1;
-    } else {
-        return_vec.at(j) = pre_nums.at(j - 1) + pre_nums.at(j);
-    }
-
-    return return_vec;
-}
-
 int main() {
-
     int n;
     cin >> n;
 
-    vector<vector<int>> ans(n, vector<int>(n));
-    vector<int> pre_nums(n, 0);
-
+    vector<vector<int>> data(n); //ジャグ配列
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i + 1; j++) {
-            ans.at(i).at(j) = print_n(i, j, pre_nums);
+            if (j == 0 || i == j) {
+                data.at(i).push_back(1);
+                if (j == i || i == 0) {
+                    cout << 1 << endl;
+                } else {
+                    cout << 1 << " ";
+                }
+
+            } else {
+                int push_num = data.at(i-1).at(j-1) + data.at(i-1).at(j);
+                data.at(i).push_back(push_num);
+                cout << push_num << " ";
+            }
         }
     }
-
-
 }
