17f03479cb4e64484dfe9ed1d4cefcfdbb4714ec
shion noguchi <shion24stat@gmail.com>
2022-06-15 20:14:13 +0000

    update

--- atcoder/ABC251/At_Most_3_judge_ver.cpp
+++ atcoder/ABC251/At_Most_3_judge_ver.cpp
@@ -6,49 +6,44 @@ int main() {
     int n, w;
     cin >> n >> w;
 
-    vector<int> a(n);
-    for (int i = 0; i < n - 1; i++) {
+    vector<int> a(n + 1);
+    a.at(0) = 0;
+    for (int i = 1; i < n + 1; i++) {
         cin >> a.at(i);
     }
 
     int counter = 0;
-    int num1, num2, num3;
-    for (int i = 0; i < n ; i++) {
-        for (int j = 0; j < n ; j++) {
-            if (i == j) {
-                break;
+    if (n == 1) {
+        counter++;
+    } else if (n == 2) {
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i != j) {
+                    int num1 = a.at(i);
+                    int num2 = a.at(j);
+
+                    if (num1 + num2 <= w) {
+                        counter++;
+                    }
+                }
             }
-            for (int k = 0; k < n ; k++) {
-                if (k == i || k == j) {
-                    break;
-                }
-
-                if (i == 0) {
-                    num1 = 0;
-                } else {
-                    num1 = a.at(i - 1);
-                }
-                
-                if (j = 0) {
-                    num2 = 0;
-                } else {
-                    num2 = a.at(j - 1);
-                }
-
-                if (k = 0) {
-                    num3 = 0;
-                } else {
-                    num3 = a.at(k - 1);
-                }
-
-                int sum = num1 + num2 + num3;
-                if (w > sum) {
-                    counter++;
+        }
+    } else {
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                for (int k = 0; k < n + 1; k++) {
+                    if ((i != j) && (j != k) && (k != i)) {
+                        int num1 = a.at(i);
+                        int num2 = a.at(j);
+                        int num3 = a.at(k);
+                        if (num1 + num2 + num3 <= w) {
+                            counter++;
+                        }
+                    }
                 }
             }
         }
     }
-
     cout << counter << endl;
 
     return 0;
