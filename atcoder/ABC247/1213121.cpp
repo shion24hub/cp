ad22620d72b96d64a9145dbe0147c3c9cb1f2616
shion noguchi <shion24stat@gmail.com>
2022-06-17 06:44:02 +0000

    first commit

---
+++ atcoder/ABC247/1213121.cpp
@@ -0,0 +1,19 @@
+//未完成
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> create() {
+    
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> s;
+    for (int i = 0; i < n; i++) {
+
+    }
+}
