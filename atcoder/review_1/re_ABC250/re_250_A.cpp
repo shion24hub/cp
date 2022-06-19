a5b262a4b3f51526849327cf04ec35dbd64b8b58
shion noguchi <shion24stat@gmail.com>
2022-06-19 04:00:26 +0000

    first commit

---
+++ atcoder/review_1/re_ABC250/re_250_A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int h, w, r, c;
+    cin >> h >> w >> r >> c;
+    
+    int ans = 0;
+    if (r - 1 >= 1) ans++;
+    if (r + 1 <= h) ans++;
+    if (c - 1 >= 1) ans++;
+    if (c + 1 <= w) ans++;
+
+    cout << ans << endl; 
+
+    return 0;
+}
