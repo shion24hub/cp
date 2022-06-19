4eaddbbaddcca9d4edf2e175849ab4f0d448b208
shion noguchi <shion24stat@gmail.com>
2022-06-19 04:00:47 +0000

    first commit

---
+++ atcoder/review_1/re_ABC253/re_253_C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int q;
+    cin >> q;
+
+    vector<int> indi_query(3);
+    int input_check;
+    vector<vector<int>> query(q);
+    for ()
+
+
+    return 0;
+}
