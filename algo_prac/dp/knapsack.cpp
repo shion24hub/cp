1da5bdbe6d8bef3c1d2bd2d102b8647a1b9f6fb6
shion noguchi <shion24stat@gmail.com>
2022-06-20 07:47:02 +0000

    complete

--- algo_prac/dp/knapsack.cpp
+++ algo_prac/dp/knapsack.cpp
@@ -27,7 +27,16 @@ int main() {
     //DPテーブル
     vector<vector<int>> dp(110, vector<int>(10010));
 
-    //
+    for (int i = 0; i <= w; i++) dp.at(0).at(i) = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= w; w++) {
+            if (j >= weight.at(i)) dp.at(i + 1).at(j) = max(dp.at(i).at(j - weight.at(i)) + value.at(i), dp.at(i).at(j));
+            else dp.at(i + 1).at(j) = dp.at(i).at(w);
+        }
+    }
+
+    cout << dp.at(n).at(w) << endl;
 
 
     return 0;
