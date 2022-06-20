157340d8bf3b9307eefda2c722a4ee78d5f46d00
shion noguchi <shion24stat@gmail.com>
2022-06-20 05:29:48 +0000

    first commit

---
+++ atcoder/ABC240/Jumping_Takahashi.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) cin >> a.at(i) >> b.at(i);
+
+    
+
+    return 0;
+}
