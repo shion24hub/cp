321bc357a4f33a3a3e3a633733b996008e40d4d6
shion noguchi <shion24stat@gmail.com>
2022-06-21 03:49:03 +0000

    first commit

---
+++ atcoder/review_1/re_244_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    cout << s.at(s.size() - 1) << endl;
+
+
+    return 0;
+}
