880eed41da6e7295f138f6b95d0f3c022279497e
shion noguchi <shion24stat@gmail.com>
2022-06-21 03:48:17 +0000

    first commit

---
+++ atcoder/ABS/ABS_1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int a, b, c;
+    string s;
+
+    cin >> a >> b >> c >> s;
+
+    cout << a + b + c << " " << s << endl;
+
+    return 0;
+}
