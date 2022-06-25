d7c9100a111212026462882e8b76e7a42e9ccedb
shion noguchi <shion24stat@gmail.com>
2022-06-25 11:37:19 +0000

    first commit

---
+++ atcoder/ABC231/Water_Pressure.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+int main() {
+    
+    float d;
+    cin >> d;
+
+    float ans = d / 100;
+
+    printf("%.10f", ans);
+
+    return 0;
+}
