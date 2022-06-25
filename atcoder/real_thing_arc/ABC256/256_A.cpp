d37f5062985e13b532c36e816dceb3e2f85cf303
shion noguchi <shion24stat@gmail.com>
2022-06-25 11:37:27 +0000

    first commit

---
+++ atcoder/real_thing_arc/ABC256/256_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long ans = pow(2, n);
+
+    cout << ans << endl;
+
+    
+
+    return 0;
+}
