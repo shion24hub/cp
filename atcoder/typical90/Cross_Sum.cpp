db1f77edd7c7e708e8903ea5c7c89610cebff238
shion noguchi <shion24stat@gmail.com>
2022-06-26 17:13:21 +0000

    first commit

---
+++ atcoder/typical90/Cross_Sum.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+    vector<vector<int>> a(h, vector<int>(w));
+    rep(i, h) rep(j, w) cin >> a.at(i).at(j);
+    vector<int> h_sum(h);
+    vector<int> w_sum(w);
+
+    for (int i = 0; i < h; i++) {
+        int sum = 0;
+        for (int j = 0; j < w; j++) {
+            sum += a.at(i).at(j);
+        }
+        h_sum.at(i) = sum;
+    }
+
+    for (int i = 0; i < w; i++) {
+        int sum = 0;
+        for (int j = 0; j < h; j++) {
+            sum += a.at(j).at(i);
+        }
+        w_sum.at(i) = sum;
+    }
+
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            cout << h_sum.at(i) + w_sum.at(j) - a.at(i).at(j) << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
