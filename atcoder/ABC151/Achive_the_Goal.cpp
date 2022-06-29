78a8bbfe1343bb767d85f522a6d6e81bd57ac132
shion noguchi <shion24stat@gmail.com>
2022-06-29 15:12:03 +0000

    first commit

---
+++ atcoder/ABC151/Achive_the_Goal.cpp
@@ -0,0 +1,95 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+int main() {
+    int n, k, m;
+    cin >> n >> k >> m;
+    int sum = 0;
+    rep(i, n-1) {
+        int a;
+        cin >> a;
+        sum += a;
+    }
+
+    int ans = (n * m) - sum;
+    if (ans <= k) {
+        if (ans < 0) cout << 0 << endl;
+        else cout << ans << endl;
+    } else {
+        cout << -1 << endl;
+    }
+
+
+    return 0;
+}
