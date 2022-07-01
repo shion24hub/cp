fc0e08eda13154a6a8b936b704eab9acecbbec73
shion noguchi <shion24stat@gmail.com>
2022-07-01 14:55:40 +0000

    correct

--- atcoder/ABC246/Coupon.cpp
+++ atcoder/ABC246/Coupon.cpp
@@ -1,42 +1,113 @@
+#define _GLIBCXX_DEBUG
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
 
 int main() {
-
-    int n, k, x;
+    
+    ll n, k, x;
     cin >> n >> k >> x;
+    vector<ll> a(n);
+    rep(i, n) cin >> a.at(i);
 
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a.at(i);
-    }
-
-    //aをソートする。
     sort(a.begin(), a.end());
     reverse(a.begin(), a.end());
 
-    for (int i = 0; i < n; i ++) {
-        if ((k > 0) && (a.at(i) > x)) {
-            a.at(i) = a.at(i) - (a.at(i) / x) * x;
-            k --;
+    rep(i, n) {
+        ll quo = a.at(i) / x;
+        quo = min(k, quo);
+        if ((quo >= 1) && (k >= 1)) {
+            k -= quo;
+            a.at(i) = a.at(i) - (x * quo);
         }
     }
 
-    sort(a.begin(), a.end());
-    reverse(a.begin(), a.end());
+    if (k >= 1) {
+        sort(a.begin(), a.end());
+        reverse(a.begin(), a.end());
 
-    int total = 0;
-    for (int i = 0; i < n; i++) {
-        if (k > 0) {
+        for (int i = 0; i < n; i++) {
+            if (k == 0) break;
             a.at(i) = 0;
-            k --;
+            k -= 1;
         }
-        cout << a.at(i) << endl;
-        total += a.at(i);
     }
 
-    cout << total << endl;
+    ll ans = 0;
+    rep(i, n) {
+        ans += a.at(i);
+    }
 
+    cout << ans << endl;
 
     return 0;
 }
