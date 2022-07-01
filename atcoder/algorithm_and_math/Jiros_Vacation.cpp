9d3e641e21f8841a536f0d733ea91ff3a2957e64
shion noguchi <shion24stat@gmail.com>
2022-07-01 11:52:28 +0000

    first commit

---
+++ atcoder/algorithm_and_math/Jiros_Vacation.cpp
@@ -0,0 +1,91 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    vector<double> a(n), b(n);
+    rep(i, n) cin >> a.at(i);
+    rep(i, n) cin >> b.at(i);
+
+    double ans = 0;
+    double prob1 = 1.0 / 3.0, prob2 = 2.0 / 3.0;
+    rep(i, n) {
+        ans += prob1 * a.at(i) + prob2 * b.at(i);
+    }
+
+    printf("%.10f", ans);
+
+    return 0;
+}
