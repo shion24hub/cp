c1046d62e0ec7616469d9fcb193ae4e50d1f6991
shion noguchi <shion24stat@gmail.com>
2022-07-01 11:52:07 +0000

    first commit

---
+++ atcoder/algorithm_and_math/Choose_Cards_3.cpp
@@ -0,0 +1,97 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> a(200009);
+    for (int i = 1; i <= n; i++) cin >> a.at(i);
+
+    vector<ll> counter(100009, 0);
+
+    for (int i = 0; i <= n; i++) counter.at(a.at(i)) += 1;
+
+    ll ans = 0;
+    for (int i = 0; i <= 49999; i++) {
+        ans += counter.at(i) * counter.at(100000 - i);
+    }
+
+    ans += counter.at(50000) * (counter.at(50000) - 1) / 2;
+
+    cout << ans << endl; 
+
+
+
+    return 0;
+}
