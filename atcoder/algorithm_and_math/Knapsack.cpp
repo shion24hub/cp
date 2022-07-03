4eac719d19afb49ab5e76e165c8bdde963c9e1ae
shion noguchi <shion24stat@gmail.com>
2022-07-03 11:13:10 +0000

    first commit

---
+++ atcoder/algorithm_and_math/Knapsack.cpp
@@ -0,0 +1,101 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+int main() {
+    int n, w;
+    cin >> n >> w;
+    vector<int> weight(n), value(n);
+    rep(i, n) cin >> weight.at(i) >> value.at(i);
+
+    vector<vector<int>> dp(n + 1, vector<int>(w + 1, -1));
+    dp.at(0).at(0) = 0;
+    // for (int i = 1; i < w + 1; i++) {
+    //     dp.at(0).at(i) = -1;
+    // }
+
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 0; j < w + 1; j++) {
+            int ref = dp.at(i - 1).at(j);
+            //dp.at(i).at(j) = ref; 
+            if (ref >= 0) {
+                if (j + weight.at(i) <= w + 1) {
+                    dp.at(i).at(j + weight.at(i)) = max(ref + value.at(i), ref);
+                }
+            }
+        }
+    }
+
+
+    return 0;
+}
