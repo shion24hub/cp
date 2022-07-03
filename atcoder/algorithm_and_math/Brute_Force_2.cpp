786a8a54c19473204749905b2b8080f4cbd06d38
shion noguchi <shion24stat@gmail.com>
2022-07-03 15:18:09 +0000

    correct

--- atcoder/algorithm_and_math/Brute_Force_2.cpp
+++ atcoder/algorithm_and_math/Brute_Force_2.cpp
@@ -76,29 +76,22 @@ int main() {
 
     int n, s;
     cin >> n >> s;
-    vector<int> a(n);
-    rep(i, n) cin >> a.at(i);
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i ++) cin >> a.at(i);
 
-    vector<vector<bool>> dp(n, vector<bool>(s + 1, false));
-    dp.at(0).at(0) = true;
-    if (a.at(0) <= s) {
-        dp.at(0).at(a.at(0)) = true;
-    }
+    vector<vector<int>> dp(n + 1, vector<int>(s + 1));
+    dp[0][0] = 1;
+    for (int i = 1; i <= s; i++) dp[0][i] = 0;
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < s + 1; j++) {
-            if (dp.at(i - 1).at(j) == true) {
-                dp.at(i).at(j) = true;
-            }
-
-            if (a.at(i) <= s) {
-                dp.at(i).at(j) = true;
-            }
-
-            
+    for (int i = 1; i <= n; i ++) {
+        for (int j = 0; j <= s; j ++) {
+            if (j < a[i]) dp[i][j] = dp[i - 1][j];
+            if (j >= a[i]) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - a[i]]);
         }
     }
 
+    if (dp[n][s]) cout << "Yes" << endl;
+    else cout << "No" << endl;
 
     return 0;
 }
