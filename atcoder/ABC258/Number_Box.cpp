cec49cbb0cf1e4da47e45db0e804d30ebba7ee01
shion noguchi <shion24stat@gmail.com>
2022-07-03 11:12:59 +0000

    first commit

---
+++ atcoder/ABC258/Number_Box.cpp
@@ -0,0 +1,110 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+//8方向へのインデックスの変化を網羅している。
+const int di[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dj[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> a(n, vector<int>(n));
+    rep(i, n) {
+        string s;
+        cin >> s;
+        rep(j, n) {
+            a[i][j] = s[j] - '0';
+        }
+    }
+
+    ll ans = 0;
+
+    rep(si, n) rep(sj, n) rep(v, 8) {
+        int i = si, j = sj;
+        ll x = 0;
+        rep(k, n) {
+            x = x * 10 + a[i][j];
+            i += di[v];
+            j += dj[v];
+            i = (i + n) % n;
+            j = (j + n) % n;
+        }
+        ans = max(ans, x);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
