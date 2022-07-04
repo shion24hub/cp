6e12f549d4654b023c4eec109baf54b8532376c8
shion noguchi <shion24stat@gmail.com>
2022-07-04 14:00:56 +0000

    first commit

---
+++ atcoder/ABC257/1D_Pawn.cpp
@@ -0,0 +1,111 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+int main() {
+    int n, k, q;
+    cin >> n >> k >> q;
+    vector<int> a(k), l(q);
+    rep(i, k) cin >> a.at(i);
+    rep(i, q) cin >> l.at(i);
+
+    vector<bool> masu(n, false);
+    rep(i, k) {
+        masu[a[i] - 1] = true;
+    }
+
+    rep(i, q) {
+        int num = l[i];
+        int counter = 0;
+        int target_ind;
+        rep(j, n) {
+            if (masu[j]) counter ++;
+            if (counter == num) target_ind = j - 1; 
+        }
+        //cout << "target_ind = " << target_ind << endl;
+        if (target_ind != n - 1) {
+            if (masu[target_ind + 1] != true) {
+                masu[target_ind] = false;
+                masu[target_ind + 1] = true;
+            } 
+        } 
+    }
+
+    rep(i, n) {
+        if (masu[i]) cout << i + 1 << " ";
+    }
+    cout << endl;
+    
+
+    return 0;
+}
