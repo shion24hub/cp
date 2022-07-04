cd7a04cb84b927c5f4208ae1c21e29075b67a3b1
shion noguchi <shion24stat@gmail.com>
2022-07-04 14:59:30 +0000

    first commit

---
+++ atcoder/ABC256/Batters.cpp
@@ -0,0 +1,109 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i, n) cin >> a[i];
+    int p = 0;
+    vector<bool> masu(4, false);
+
+    rep(i, n) {
+        vector<int> database;
+        masu[0] = true;
+        int addition = a[i];
+        rep(j, 4) {
+            if (masu[j]) {
+                if ((j + addition) < 4) {
+                    database.push_back(j + addition);
+                } else {
+                    p ++;
+                }
+            }
+        }
+
+        rep(k, 4) {
+            masu[k] = false;
+        }
+
+        for (int l = 0; l < database.size(); l++) {
+            masu[database[l]] = true;
+        }
+    }
+
+    cout << p << endl;
+
+    return 0;
+}
