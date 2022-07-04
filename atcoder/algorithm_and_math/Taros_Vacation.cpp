76c2a45dbf939f851257d028e910277d58be6219
shion noguchi <shion24stat@gmail.com>
2022-07-04 14:01:04 +0000

    first commit

---
+++ atcoder/algorithm_and_math/Taros_Vacation.cpp
@@ -0,0 +1,106 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+void pdebag() {
+    cout << "hi" << endl;
+}
+
+void pintvec(vector<int> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<int> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pstrvec(vector<string> vec, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print vector<string> from here" << endl;
+    }
+    for (int i = 0; i < vec.size(); i++) cout << vec.at(i) << endl;
+}
+
+void pismap(map<int, string> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, string> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void psimap(map<string, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<string, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+void piimap(map<int, int> m, int mode_num = 0) {
+    if (mode_num == 1) {
+        cout << endl;
+        cout << "print map<int, int> from here" << endl;
+    }
+    for (auto p: m) {
+        auto key = p.first;
+        auto value = p.second;
+        if (mode_num == 1) {
+            cout << key << " => " << value << endl;
+        } else {
+            cout << key << " " << value << endl;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i ++) cin >> a[i];
+
+    vector<vector<int>> dp(2, vector<int>(n));
+    dp[0][0] = a[0];
+    dp[1][0] = a[0];
+    dp[0][1] = a[1];
+    dp[1][1] = a[1];
+    for (int i = 2; i < n; i++) {
+        dp[0][i] = dp[1][i - 2] + a[i];
+        dp[1][i] = max(dp[1][i - 1], dp[0][i]);
+    }
+
+    // for (int i = 0; i < n; i++) {
+    //     cout << dp[0][i] << " ";
+    // }
+    // cout << endl;
+
+    // for (int i = 0; i < n; i++) {
+    //     cout << dp[1][i] << " ";
+    // }
+
+    // cout << endl;
+
+    cout << dp[1][n - 1] << endl;
+
+
+    return 0;
+}
